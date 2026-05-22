e46bd7c99673e86171b9fca4abfd848f21442d51
Vinicius-Boschi <vinicius.yg@gmail.com>
2026-05-22 19:46:03 -0300

    prepare deploy backend render

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ import lyricsHandler from "./api/lyrics/index.js"
 dotenv.config()
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.get("/api/lyrics", (req, res) => lyricsHandler(req, res))
 
 app.listen(PORT, () => {
-  console.log(`API rodando em http://localhost:${PORT}`)
+  console.log(`API rodando na porta ${PORT}`)
 })
