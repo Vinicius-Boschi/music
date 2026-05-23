2c3b517ba7300d1ce2cf0cbfb30c66762d85fac1
Vinicius-Boschi <vinicius.yg@gmail.com>
2026-05-22 21:21:06 -0300

    add deezer proxy

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ const PORT = process.env.PORT || 3001
 app.use(cors())
 app.get("/api/lyrics", (req, res) => lyricsHandler(req, res))
 
+app.get("/api/deezer/*", async (req, res) => {
+  try {
+    const path = req.params[0]
+
+    const response = await fetch(
+      `https://api.deezer.com/${path}`
+    )
+
+    const data = await response.json()
+
+    res.json(data)
+  } catch (err) {
+    console.error(err)
+
+    res.status(500).json({
+      error: err.message,
+    })
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`API rodando na porta ${PORT}`)
 })
