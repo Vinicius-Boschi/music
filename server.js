6b92787850c8af580ca6323b304db35ae2d0eb78
Vinicius-Boschi <vinicius.yg@gmail.com>
2026-09-02 17:19:09 -0300

    Removendo código de teste

--- server.js
+++ server.js
@@ -11,35 +11,6 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.get("/api/lyrics", (req, res) => lyricsHandler(req, res));
 
-app.get("/api/test-deezer-chart", async (req, res) => {
-  try {
-    const urls = [
-      "https://api.deezer.com/chart/0/artists",
-      "https://api.deezer.com/chart/31/artists",
-      "https://api.deezer.com/chart/BR/artists",
-    ]
-
-    const results = await Promise.all(
-      urls.map(async (url) => {
-        const response = await fetch(url)
-        const data = await response.json()
-
-        return {
-          url,
-          status: response.status,
-          firstArtists: data?.data?.slice(0, 5).map((artist) => artist.name),
-          error: data?.error || null,
-        }
-      })
-    )
-
-    res.json(results)
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: err.message })
-  }
-})
-
 app.get(/^\/api\/deezer\/(.+)/, async (req, res) => {
   try {
     const path = req.params[0];
