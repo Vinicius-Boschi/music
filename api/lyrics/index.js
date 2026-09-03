26685ae68c84444d0f9e9699177dd2410edbf303
Vinicius-Boschi <vinicius.yg@gmail.com>
2026-09-03 16:31:53 -0300

    Adicionando log de erro

--- api/lyrics/index.js
+++ api/lyrics/index.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 
     res.status(200).json({ lyrics })
   } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: err.message })
+    console.error("Erro Genius:",err)
+    res.status(500).json({ error: err.message, details: err.response?.data || null })
   }
 }
